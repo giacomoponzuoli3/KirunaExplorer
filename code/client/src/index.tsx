c06724892fd3132c09d2cd85844886c98f29618f
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2024-10-24 12:39:50 +0200

    Setup demo

--- code/client/src/index.tsx
+++ code/client/src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-
+import "./index.css";
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
