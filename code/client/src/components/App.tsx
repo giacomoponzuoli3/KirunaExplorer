575f9e6fe9447602dd3cd37584af924d32e80d00
dimitrievskamilica <milica.dimitrievska@student.um.si>
2024-10-27 19:31:39 +0100

    implement a GUI to view a document

--- code/client/src/components/App.tsx
+++ code/client/src/components/App.tsx
@@ -3,14 +3,68 @@ import {Navbar} from "./Navbar"
 import API from '../API/API';
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import {Login} from './Login'; // Assicurati che il percorso sia corretto
+import { HomePage } from './HomePage';
 import { NotFoundLayout } from './NotFoundLayout';
 import { Container } from 'react-dom';
+import { Document } from '../models/document';
 
 function App() {
   const [user, setUser] = useState<any>('');
   const [isLogged, setIsLogged] = useState<any>(false);
   const [message, setMessage] = useState<any>('');
+  const [documents, setDocuments] = useState<Document[]>([]);
 
+  useEffect(() => {
+    //API call to get all the documents so we can display them
+    //const docs = API.getAllDocuments();
+    //setDocuments(docs)
+
+    //the following lines are temporary until the fronend and back end are not integrated
+    const docs = [
+      new Document(
+          "Compilation of responses 'So what the people of Kiruna think?'",
+          "Kiruna kommun/Residents",
+          "Text",
+          "2007",
+          "Informative document",
+          "Swedish",
+          null,
+          "This document is a compilation of the responses to the survey 'What is your impression of Kiruna?'. From the citizens' responses to this last part of the survey, it is evident that certain buildings, such as the Kiruna Church, the Hjalmar Lundbohmsgården, and the Town Hall, are considered of significant value to the population. The municipality views the experience of this survey positively, to the extent that over the years it will propose various consultation opportunities."
+      ),
+      new Document(
+          "Detail plan for Bolagsomradet Gruvstadspark",
+          "Kiruna kommun",
+          "1 : 8.000",
+          "20/10/2010",
+          "Prescriptive document",
+          "Swedish",
+          "1-32",
+          "This is the first of 8 detailed plans located in the old center of Kiruna, aimed at transforming the residential areas into mining industry zones to allow the demolition of buildings. The area includes the town hall, the Ullspiran district, and the A10 highway, and it will be the first to be dismantled. The plan consists, like all detailed plans, of two documents: the area map that regulates it, and a text explaining the reasons that led to the drafting of the plan with these characteristics. The plan gained legal validity in 2012."
+      ),
+      new Document(
+          "Construction of Block 1 begins",
+          "LKAB",
+          "blueprints/effects",
+          "06/2019",
+          "Material effect",
+          null,
+          null,
+          "Simultaneously with the start of construction on the Aurora Center, work also began on Block 1, another mixed-use building overlooking the main square and the road leading to old Kiruna. These are the first residential buildings in the new town."
+      ),
+      new Document(
+          "Development Plan",
+          "Kiruna kommun/White Arkitekter",
+          "1 : 7,500",
+          "17/03/2014",
+          "Design document",
+          "Swedish",
+          "111",
+          null
+      )
+  ];
+  setDocuments(docs);
+
+  }, []);
 
   const handleBack = () => {
     setMessage('');
@@ -61,7 +115,7 @@ function App() {
             <Outlet/>
           </>
         }>
-          <Route index />
+          <Route index element={<HomePage documents={documents}/>}/>
           <Route path="/login" element={<Login message={message} isLogged={isLogged} login={handleLogin} handleBack={handleBack}/>} />
           <Route path="*" element={<NotFoundLayout/>} />
           {/* Aggiungi altre route come la dashboard */}
---
+++ code/client/src/models/document.ts
@@ -0,0 +1,58 @@
+/**
+ * Represents a document.
+ */
+class Document {
+    title: string
+    stakeHolders: string
+    scale: string
+    issuanceDate: string
+    type: string
+    language: string | null
+    pages: string | null
+    description: string | null
+
+    /**
+     * Creates a new instance of the Document class.
+     * @param title - The title of the document.
+     * @param stakeHolders - The stakeholders of the document.
+     * @param scale - The scale of the document.
+     * @param issuanceDate - The issuance date of the document.
+     * @param type - The type of the document.
+     * @param language - The language of the document.
+     * @param pages - The number of pages in the document.
+     * @param description - The description of the document.
+     */
+    constructor(
+        title: string,
+        stakeHolders: string,
+        scale: string,
+        issuanceDate: string,
+        type: string,
+        language: string | null = null,
+        pages: string | null = null,
+        description: string | null = null
+    ) {
+        this.title = title
+        this.stakeHolders = stakeHolders
+        this.scale = scale
+        this.issuanceDate = issuanceDate
+        this.type = type
+        this.language = language
+        this.pages = pages
+        this.description = description
+    }
+}
+/**
+ * Represents the type of a document.
+ * The values present in this enum are the only valid values for the type of a document.
+ */
+enum Type {
+    INFORMATIVE = "Informative document",
+    PRESCRIPTIVE = "Prescriptive document",
+    DESIGN = "Design document",
+    TECHNICAL = "Technical document",
+    MATERIAL = "Material effect"
+}
+
+
+export { Document, Type}
