44c2ef7bb738208db78d84fb06fb9ffd03a60322
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2024-10-28 18:27:35 +0100

    Modify login page

--- code/client/src/components/Navbar.tsx
+++ code/client/src/components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar(props: any) {
     <Disclosure as="nav" className="bg-gradient-to-r from-orange-400 to-yellow-500">
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="flex flex-1 items-center justify-left sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
               <img
                 alt="Kimura Company"
