087304c30ec8e84c695e40c2eddb1cb89065260a
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2024-11-15 10:33:08 +0100

    fix

--- code/server/jest.config.js
+++ code/server/jest.config.js
@@ -4,11 +4,4 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/**test**/**/*.test.ts'],
     reporters: ["default"],
-    collectCoverage: true, // Abilita la raccolta della coverage
-  coverageDirectory: './coverage', // La cartella dove Jest salverà i report di coverage
-  coverageReporters: ['lcov', 'text'], // Genera il report in formato LCOV e anche una sintesi nel terminale
-  coveragePathIgnorePatterns: [
-    '/node_modules/', // Ignora la cartella node_modules
-    '/dist/', // Ignora i file di build (se li hai)
-  ],
 }
