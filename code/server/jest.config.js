5affa74930928720f75c51648d86f2fac514b62b
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2024-11-15 09:35:10 +0100

    delete sonarQube

--- code/server/jest.config.js
+++ code/server/jest.config.js
@@ -3,5 +3,12 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/**test**/**/*.test.ts'],
-    reporters: ["default"]
+    reporters: ["default"],
+    collectCoverage: true, // Abilita la raccolta della coverage
+  coverageDirectory: './coverage', // La cartella dove Jest salverà i report di coverage
+  coverageReporters: ['lcov', 'text'], // Genera il report in formato LCOV e anche una sintesi nel terminale
+  coveragePathIgnorePatterns: [
+    '/node_modules/', // Ignora la cartella node_modules
+    '/dist/', // Ignora i file di build (se li hai)
+  ],
 }
