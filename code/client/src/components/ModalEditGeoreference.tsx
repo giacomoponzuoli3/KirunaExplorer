36313fc720b5896ffb49fb313a3aa08356787b5f
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2024-11-16 11:28:12 +0100

    finished frontend view documents on the map

--- code/client/src/components/ModalEditGeoreference.tsx
+++ code/client/src/components/ModalEditGeoreference.tsx
@@ -16,9 +16,9 @@ interface ModalEditGeoreferenceProps {
 }
 
 const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
-  documentCoordinates, 
-  onClose, 
-  refreshDocuments, 
+  documentCoordinates,
+  onClose,
+  refreshDocuments,
   refreshDocumentsCoordinates
 }) => {
   const [selectedPosition, setSelectedPosition] = useState<LatLng[] | null>(null);
@@ -26,20 +26,77 @@ const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
   const [error, setError] = useState<string | null>(null);
   const [useMunicipalArea, setUseMunicipalArea] = useState(false);  // Stato per la checkbox
 
-  // Mappa figlio: Usa useMap per ottenere il contesto della mappa
+  // Default coordinate of municipal city
+  const coordinatesCity: L.LatLng[] = [
+    L.latLng(67.8753332, 20.1841097),
+    L.latLng(67.8749453, 20.1866846),
+    L.latLng(67.8738462, 20.1880579),
+    L.latLng(67.8731996, 20.1878862),
+    L.latLng(67.8710012, 20.193551),
+    L.latLng(67.8689318, 20.1969843),
+    L.latLng(67.8659569, 20.1988725),
+    L.latLng(67.8638871, 20.2023058),
+    L.latLng(67.8620112, 20.204709),
+    L.latLng(67.8603939, 20.205224),
+    L.latLng(67.8586471, 20.2011041),
+    L.latLng(67.8573531, 20.1971559),
+    L.latLng(67.857159, 20.1923494),
+    L.latLng(67.8563826, 20.1921778),
+    L.latLng(67.8561885, 20.1944093),
+    L.latLng(67.8541826, 20.1870279),
+    L.latLng(67.8528883, 20.182908),
+    L.latLng(67.8534707, 20.1793031),
+    L.latLng(67.8552179, 20.1798181),
+    L.latLng(67.8567061, 20.1808481),
+    L.latLng(67.8577414, 20.1823931),
+    L.latLng(67.858259, 20.183938),
+    L.latLng(67.8583236, 20.1868562),
+    L.latLng(67.857159, 20.1873712),
+    L.latLng(67.8577414, 20.1894312),
+    L.latLng(67.8588412, 20.1880579),
+    L.latLng(67.8596176, 20.185483),
+    L.latLng(67.8618171, 20.1880579),
+    L.latLng(67.8623993, 20.1875429),
+    L.latLng(67.8620759, 20.185483),
+    L.latLng(67.8633696, 20.1851396),
+    L.latLng(67.8640811, 20.1823931),
+    L.latLng(67.8634343, 20.1803331),
+    L.latLng(67.8590353, 20.17381),
+    L.latLng(67.8598117, 20.1688318),
+    L.latLng(67.8602645, 20.163167),
+    L.latLng(67.8587765, 20.150464),
+    L.latLng(67.8428555, 20.1463442),
+    L.latLng(67.8337899, 20.2012758),
+    L.latLng(67.8384526, 20.2012758),
+    L.latLng(67.8289966, 20.2541475),
+    L.latLng(67.8229065, 20.2901964),
+    L.latLng(67.8384526, 20.3166323),
+    L.latLng(67.8381936, 20.3561144),
+    L.latLng(67.851141, 20.3619509),
+    L.latLng(67.8604586, 20.3066759),
+    L.latLng(67.8781777, 20.2129488),
+    L.latLng(67.8753332, 20.1841097)
+  ];
+
+  // Determina se le coordinate rappresentano un poligono o un punto
+  const isPolygon = documentCoordinates.coordinates && documentCoordinates.coordinates.length > 1;
+
+  console.log(selectedPosition);
+
   const handleUpdate = async () => {
-    if (selectedPosition != null) {
+    console.log("update: " + selectedPosition);
+    if (selectedPosition != null && selectedPosition.length !== 0) {
       setIsLoading(true);
       setError(null);
 
       try {
         // Fai la chiamata API per aggiornare le coordinate del documento
         await API.updateDocumentCoordinates(documentCoordinates.id, selectedPosition);
-        
+
         // Dopo aver aggiornato, ricarica i documenti
         refreshDocuments();
         refreshDocumentsCoordinates();
-  
+
         // Chiudi il modal dopo il salvataggio
         onClose();
       } catch (err) {
@@ -54,6 +111,7 @@ const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
   // Funzione per gestire il cambio di stato della checkbox
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUseMunicipalArea(event.target.checked);
+    setSelectedPosition(coordinatesCity);
   };
 
   // Impostiamo il map component all'interno del modal
@@ -73,15 +131,23 @@ const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
         <form>
           {/* Cambia il contenuto del modal */}
           <div className="mb-4">
-            <label htmlFor="location" className="block text-sm font-medium text-gray-800 mb-2 hover:text-blue-500">
-              <span className="text-gray-600">Actual latitude: </span>
-              <span className="font-semibold text-blue-600">{documentCoordinates.coordinates[0].latitude}</span>
-            </label>
+            {isPolygon ? (
+              <div className="flex items-center text-blue-600 mb-3">
+              <span className="font-semibold">This document is associated with an area of the city, and not a single point.</span>
+            </div>
+            ) : (
+              <>
+                <label htmlFor="location" className="block text-sm font-medium text-gray-800 mb-2 hover:text-blue-500">
+                  <span className="text-gray-600">Actual latitude: </span>
+                  <span className="font-semibold text-blue-600">{documentCoordinates.coordinates[0].latitude}</span>
+                </label>
 
-            <label htmlFor="location" className="block text-sm font-medium text-gray-800 mb-4 hover:text-blue-500">
-              <span className="text-gray-600">Actual longitude: </span>
-              <span className="font-semibold text-blue-600">{documentCoordinates.coordinates[0].longitude}</span>
-            </label>
+                <label htmlFor="location" className="block text-sm font-medium text-gray-800 mb-4 hover:text-blue-500">
+                  <span className="text-gray-600">Actual longitude: </span>
+                  <span className="font-semibold text-blue-600">{documentCoordinates.coordinates[0].longitude}</span>
+                </label>
+              </>
+            )}
 
             <label htmlFor="location" className="block text-sm font-medium text-gray-700 mb-2">
               Select New Georeference
@@ -92,7 +158,6 @@ const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
               <MapContainer style={{ height: '100%' }}>
                 {/* TileLayer per visualizzare la mappa */}
                 <SetMapViewEdit
-                  selectedPosition={selectedPosition}
                   setSelectedPosition={setSelectedPosition}
                   useMunicipalArea={useMunicipalArea}  // Passa lo stato per disabilitare la mappa
                 />
@@ -102,7 +167,6 @@ const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
 
           {/* Toggle switch per l'area municipale */}
           <div className="mt-4 flex items-center mb-5">
-            {/* Checkbox a sinistra della scritta */}
             <label htmlFor="municipal-area-checkbox" className="relative inline-flex items-center cursor-pointer">
               <input
                 type="checkbox"
@@ -114,10 +178,9 @@ const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
               <span className="w-11 h-6 bg-gray-300 rounded-full peer peer-checked:bg-blue-500 transition"></span>
               <span className="absolute left-1 top-1 w-4 h-4 bg-white rounded-full peer-checked:translate-x-5 transition"></span>
             </label>
-            {/* Scritta accanto al toggle */}
             <span className="ml-3 text-sm text-gray-600">The entire municipal area</span>
           </div>
-          
+
           <div className="mt-6 flex justify-between">
             <button
               type="button"
@@ -133,7 +196,7 @@ const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
               className="px-4 py-2 bg-blue-950 text-white rounded-lg hover:bg-blue-700 focus:outline-none"
             >
               {isLoading ? 'Updating...' : 'Update'}
-            </button>   
+            </button>
           </div>
         </form>
       </div>
@@ -141,9 +204,10 @@ const ModalEditGeoreference: React.FC<ModalEditGeoreferenceProps> = ({
   );
 };
 
+
 // Componente per la costruzione della mappa
 function SetMapViewEdit(props: any) {
-  const { selectedPosition, setSelectedPosition, useMunicipalArea } = props;
+  const { setSelectedPosition, useMunicipalArea } = props;
   const map = useMap(); // Ottieni l'istanza della mappa
 
   // Coordinate di esempio (come punto centrale della mappa)
@@ -151,8 +215,8 @@ function SetMapViewEdit(props: any) {
 
   // Limiti della mappa per Kiruna
   const kirunaBounds = new LatLngBounds(
-    [67.3556, 17.8994], // Sud-ovest
-    [69.0592, 23.2858]  // Nord-est
+    [67.790390, 20.416509],  // Sud-ovest
+    [67.889194, 20.050656]   // Nord-est
   );
 
   // Icona personalizzata per il marker
@@ -208,16 +272,24 @@ function SetMapViewEdit(props: any) {
 
   // Se la checkbox è selezionata, disabilita la mappa
   useEffect(() => {
+
     if (useMunicipalArea) {
       map.doubleClickZoom.disable();
       map.dragging.disable();
       map.touchZoom.disable();
       map.scrollWheelZoom.disable();
     } else {
+      // Rimuovere il vecchio marker, se presente
+      map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          map.removeLayer(layer);
+        }
+      });
       map.doubleClickZoom.enable();
       map.dragging.enable();
       map.touchZoom.enable();
       map.scrollWheelZoom.enable();
+      setSelectedPosition(null);
     }
   }, [map, useMunicipalArea]);
 
