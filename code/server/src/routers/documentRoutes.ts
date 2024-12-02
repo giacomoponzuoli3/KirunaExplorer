8778be8f74ee99fa4f73252bbb7514bf39d472c3
souna-neisi <136485884+souna-neisi@users.noreply.github.com>
2024-12-02 22:34:53 +0100

    fix deleteResources api

--- code/server/src/routers/documentRoutes.ts
+++ code/server/src/routers/documentRoutes.ts
@@ -269,16 +269,16 @@ class DocumentRoutes {
             }
         );
 
-        this.router.delete(
+         this.router.delete(
             "/res/:idDoc/:name",
             this.authenticator.isLoggedIn, //error 401
             this.authenticator.isPlanner, //error 403
-            param("id").isNumeric(),
+            param("idDoc").isNumeric(),
             param("name").isString().notEmpty(),
             this.errorHandler.validateRequest,
             (req: any, res: any, next: any) => {
                 try {
-                    this.controller.deleteResource(req.params["id"], req.params["name"])
+                    this.controller.deleteResource(req.params["idDoc"], req.params["name"])
                         .then(() => res.status(200).json({ message: "Document deleted successfully" }))
                         .catch((err: Error) => next(err))
                 } catch (err) {
