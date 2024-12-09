ba4286d2732ad9cfedcf18c00021e15ad44c827b
ponzuoligiacomo@gmail.com <ponzuoligiacomo@gmail.com>
2024-12-09 09:46:08 +0100

    add diagram component

--- code/client/src/components/DiagramDocuments.tsx
+++ code/client/src/components/DiagramDocuments.tsx
@@ -1,19 +1,4 @@
 import React from 'react';
-import ReactApexChart from 'react-apexcharts';
-
-// Dati di esempio
-const data = [
-  { year: 2010, scale: "1:1000" },
-  { year: 2010, scale: "Text" },
-  { year: 2011, scale: "1:10000" },
-  { year: 2011, scale: "Concept" },
-  { year: 2012, scale: "Blueprint/effect" },
-  { year: 2012, scale: "1:5000" },
-  { year: 2013, scale: "1:1000" },
-  { year: 2013, scale: "1:10000" },
-];
-
-
 
 const DiagramDocuments = (props: any) => {
   
