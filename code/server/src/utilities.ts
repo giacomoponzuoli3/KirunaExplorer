63d9484c5ca9992af4a30cc09df5d0ed723c76ce
robym <com.robym@gmail.com>
2024-12-18 18:00:55 +0100

    Removed unused functions in utilities.ts

--- code/server/src/utilities.ts
+++ code/server/src/utilities.ts
@@ -12,23 +12,6 @@ class Utility {
     static isPlanner(user: User): boolean {
         return user.role === Role.PLANNER
     }
-    /**
-     * Checks if a user is a resident.
-     * @param {User} user - The user to check.
-     * @returns True if the user is a resident, false otherwise.
-     */
-    static isResident(user: User): boolean {
-        return user.role === Role.RESIDENT
-    }
-    /**
-     * Checks if a user is an urban developer.
-     * @param {User} user - The user to check.
-     * @returns True if the user is an urban developer, false otherwise.
-     */
-    static isDeveloper(user: User): boolean {
-        return user.role === Role.DEVELOPER
-    }
-
 }
 
 export default Utility
